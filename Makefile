d0d08ac34093d21b0a16036cf959f0daad686238
Josef Vasata <jvasata03@gmail.com>
2025-08-28 19:25:23 +0200

    Disable ECHO termios flag

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
 CC = gcc
 
+all: kilo
+
+run : kilo
+	./kilo && reset
+
 kilo: main.c
 	$(CC) main.c -o kilo -Wall -Wextra -pedantic -std=c99
--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios termios_default;
+
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &termios_default); }
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &termios_default);
+    atexit(disableRawMode);
+
+
+    struct termios t = termios_default;
+
+    t.c_lflag &= ~(ECHO); // ECHO - print pressed keys
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
+    // TCSAFLUSH - wait for pending output, discard unread input
+}
 
 int main(void) {
-    printf("Hello world!\n");
+    enableRawMode();
+    char c = 0;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+        ;
+
+    disableRawMode();
     return EXIT_SUCCESS;
 }
