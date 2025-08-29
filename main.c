5b160c4ba213892fbe693ad86ec8b706e24053e3
Josef Vasata <jvasata03@gmail.com>
2025-08-29 15:54:42 +0200

    Set all necessary flags for raw mode

--- main.c
+++ main.c
@@ -12,14 +12,25 @@ void enableRawMode() {
     tcgetattr(STDIN_FILENO, &termios_default);
     atexit(disableRawMode);
 
-    struct termios t = termios_default;
+    struct termios raw = termios_default;
 
-    // ECHO - print pressed keys
+    // ICRLN  - don't translate C-m \r into \n
+    // IXON   - disable C-s and C-q control flow
+    // BRKINT, INPCK, ISTRIP - misc.
+    // (https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html)
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    // OPOST  - disable output post processing (\n to \r\n)
+    raw.c_oflag &= ~(OPOST);
+    // CS8    - set char size to 8
+    raw.c_cflag |= CS8;
+    // ECHO   - print pressed keys
     // ICANON - read byte-by-byte instead of line-by-line
-    t.c_lflag &= ~(ECHO | ICANON);
+    // ISIG   - disable C-z and C-c (SIGTSTP, SIGINT)
+    // IEXTEN - disable C-v
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
     // TCSAFLUSH - wait for pending output, discard unread input
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main(void) {
@@ -27,9 +38,9 @@ int main(void) {
     char c = 0;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     return EXIT_SUCCESS;
